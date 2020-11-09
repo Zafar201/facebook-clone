55db5c538cbf1123483637913e337b8968e07072
Zafar201 <ayshasawaab@gmail.com>
2020-11-09 20:56:45 +0530

    working on feed

---
+++ src/Header.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search'
+import HomeIcon from '@material-ui/icons/Home'
+import  SubscriptionsOutlinedIcon   from '@material-ui/icons/SubscriptionsOutlined'
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle'
+import FlagIcon from '@material-ui/icons/Flag'
+import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined'
+import  {Avatar,IconButton}  from '@material-ui/core'
+import   AddIcon  from '@material-ui/icons/Add'
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive'
+import  ForumIcon  from '@material-ui/icons/Forum'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
+function Header() {
+    return (
+        <div className='header'>
+          <div className='header__left'>
+               <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQeqkOI9k1hiqyrhe9Andi2t0hlzColPSNiJw&usqp=CAU' alt='blaah'/>
+          
+
+            <div className='header__input'>
+              <SearchIcon/> 
+              <input type='text' placeholder="Search Facebook"/> 
+           </div>
+         </div>
+         
+          <div className='header__middle'>
+               <div className='header__option header__option--active'>
+                 <HomeIcon fontSize='large'/> 
+               </div> 
+              <div className='header__option'>
+                 <FlagIcon fontSize='large'/> 
+              </div>
+              <div className='header__option'>
+                 <SubscriptionsOutlinedIcon fontSize='large'/> 
+              </div>           
+              <div className='header__option'>
+                 <StorefrontOutlinedIcon fontSize='large'/> 
+              </div>
+               <div className='header__option'>
+                 <SupervisedUserCircleIcon fontSize='large'/> 
+               </div>
+          </div>  
+
+         <div className='header__right'>
+            <div className='header__info'>
+               <Avatar/>
+               <h4>Zafar irshad</h4>
+            </div> 
+
+           <IconButton>
+              <AddIcon/>
+           </IconButton>
+           <IconButton>
+              <ForumIcon/>
+           </IconButton>
+           <IconButton>
+              <NotificationsActiveIcon/>
+           </IconButton>
+           <IconButton>
+              <ExpandMoreIcon/>
+           </IconButton>
+         </div> 
+        </div>
+    )
+}
+
+export default Header
